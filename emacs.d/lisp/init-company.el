ec625ec66f9c072655cd7021120a6837204953aa
Charis-Nicolas Georgiou <cng_it@posteo.net>
2018-04-01 20:16:54 +0200

    Added company mode to config

--- emacs.d/lisp/init-company.el
+++ emacs.d/lisp/init-company.el
@@ -1,3 +1,9 @@
+;;; Company Mode configuration (Autocomplete)
+(use-package company
+  :ensure t
+  :config
+  :hook 'after-init-hook 'global-company-mode)
+
 (setq tab-always-indent 'complete)
 (add-to-list 'completion-styles 'initials t)
 
