97dffe075334538d82a45975a3fa8f1c6fc00145
Charis-Nicolas Georgiou <cng_it@posteo.net>
2018-04-14 10:13:28 +0200

    Verbose configuration of ivy outside swyper

--- emacs.d/lisp/init-ivy.el
+++ emacs.d/lisp/init-ivy.el
@@ -1,40 +1,59 @@
 ;;; Swiper Configuration wiht counsel and ivy
 ;;; Code:
-(use-package ivy)
-(use-package counsel)
+(use-package ivy
+  :diminish
+  :bind (("C-x b" . ivy-switch-buffer)
+         ("C-c C-r" . ivy-resume)
+         ("C-x B" . ivy-switch-buffer-other-window))
+  :custom
+  (ivy-dynamic-exhibit-delay-ms 200)
+  (ivy-height 10)
+  (ivy-initial-input-alist nil t)
+  (ivy-use-virtual-buffers t)
+  (ivy-magic-tilde nil)
+  (ivy-virtual-abbreviate 'fullpath)
+    (ivy-count-format "")
+  :config
+  (ivy-mode 1)
+  (ivy-set-occur 'ivy-switch-buffer 'ivy-switch-buffer-occur))
+(use-package counsel
+  :after ivy
+  :demand t
+  :diminish
+  :bind (("C-*" . counsel-org-agenda-headlines)
+         ("C-x C-f" . counsel-find-file))
+  :commands counse-minibuffer-history
+  :custom
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+  (define-key read-expression-map (kbd "C-r") 'counsel-expression-history))
+
+(use-package counsel-tramp
+  :commands counsel-tramp)
+
 (use-package swiper
   :bind (("C-s" . swiper)
          ("C-r" . swiper)
-         ("C-c C-r" . ivy-resume)
          ("M-x" . counsel-M-x)
          ("C-x C-f" . counsel-find-file))
   :config
   (progn
-    (ivy-mode 1)
     (setq-default
-     ivy-use-virtual-buffers t
-     ivy-virtual-abbreviate 'fullpath
-     ivy-count-format ""
-     ivy-magic-tilde nil
-     ivy-dynamic-exhibit-delay-ms 150
      magit-completing-read-function 'ivy-completing-read
-     enable-recursive-minibuffers t)
-    (global-set-key (kbd "<f6>") 'ivy-resume)
-    (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-    (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-    (global-set-key (kbd "<f1> l") 'counsel-find-library)
-    (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-    (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-    (global-set-key (kbd "C-c g") 'counsel-git)
-    (global-set-key (kbd "C-c j") 'counsel-git-grep)
-    (global-set-key (kbd "C-c k") 'counsel-ag)
-    (global-set-key (kbd "C-x l") 'counsel-locate)
-    (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-    ))
+     enable-recursive-minibuffers t)))
 
 (use-package ivy-xref
   :config
   (setq xref-show-xrefs-function 'ivy-xref-show-xrefs))
+
+
 (provide 'init-ivy)
 ;;; init-ivy.el Ends here
