f88d666f408a7ed8228797e90380a9e30e54fca0
Charis-Nicolas Georgiou <cng_it@posteo.net>
2018-04-14 10:08:16 +0200

    Better yasnippet conf usin use-package

--- emacs.d/lisp/init-yasnippet.el
+++ emacs.d/lisp/init-yasnippet.el
@@ -1,15 +1,17 @@
 ;;; Code:
 (use-package yasnippet
-  :ensure t
+  :after prog-mode
+  :defer 10
+  :diminish yas-minor-mode
   :init
   (add-hook 'after-init-hook 'yas-global-mode)
   (add-hook 'term-mode-hook #'force-yasnippet-off)
   (add-hook 'shell-mode-hook #'force-yasnippet-off)
   :config
+  (yas-global-mode 1)
   ;; (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
   ;; (setq yas-snippet-dirs '("~/.emacs.d/lisp/yasnippet/snippets"))
-  (yas-reload-all)
-  )
+  (yas-reload-all))
 (use-package yasnippet-snippets
   :ensure t)
 ;; (add-to-list 'load-path
