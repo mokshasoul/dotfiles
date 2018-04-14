77c9aa2aa1fe84a1ebe6beeb1c134e82b5c76a19
Charis-Nicolas Georgiou <cng_it@posteo.net>
2018-04-14 10:09:10 +0200

    Updated web-mode configuration

--- emacs.d/lisp/init-web.el
+++ emacs.d/lisp/init-web.el
@@ -8,10 +8,25 @@
   ("\\.as[cp]x\\'" . web-mode)
   ("\\.erb\\'" . web-mode)
   ("\\.mustache\\'" . web-mode)
-  ("\\.html?\\'" . web-mode))
+  ("\\.html?\\'" . web-mode)
+  ("\\.djhtml?\\'" . web-mode))
   :init
   (setq web-mode-code-indent-offset 2
-        web-mode-markupt-indent-offset 2))
+        web-mode-markupt-indent-offset 2
+        web-mode-enable-auto-pairing nil))
+
+;; make web-mode play nice with smartparens taken from
+;; prelude
+;; (sp-with-modes '(web-mode)
+;;   (sp-local-pair "%" "%"
+;;                  :unless '(sp-in-string-p)
+;;                  :post-handlers '(((lambda (&rest _ignored)
+;;                                      (just-one-space)
+;;                                      (save-excursion (insert " ")))
+;;                                    "SPC" "=" "#")))
+;;   (sp-local-tag "%" "<% "  " %>")
+;;   (sp-local-tag "=" "<%= " " %>")
+;;   (sp-local-tag "#" "<%# " " %>")
 ;;; EMMET is nice ;)
 (use-package emmet-mode
   :requires web-mode
