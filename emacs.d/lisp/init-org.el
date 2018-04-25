f10cc861677ab45e3de0c9ffba0e4ca8ebb6e4d7
Charis-Nicolas Georgiou <cng_it@posteo.net>
2018-04-25 22:52:56 +0200

    added more templates and configured more GTD oriented settings

--- emacs.d/lisp/init-org.el
+++ emacs.d/lisp/init-org.el
@@ -6,6 +6,7 @@
 (setq org-log-done t
       org-refile-targets '((nil :maxlevel . 4)
                            (org-agenda-files :maxlevel . 4))
+      org-refile-use-outline-path t
       org-edit-timestamp-down-means-later t
       org-archive-mark-done nil
       org-hide-emphasis-markers t
@@ -14,12 +15,20 @@
       org-fast-tag-selection-single-key 'expert
       org-html-validation-link nil
       org-export-kill-product-buffer-when-displayed t
-      org-tags-column 8
+      org-tags-column 5
+      org-directory '("~/Documents/_org/")
       org-agenda-files '("~/Documents/_org/")
       org-default-notes-file "~/Documents/_org/notes.org")
 ;; inserts full filename at top of file to link different org files
 ;; (use-package org-fstree
 ;;   :ensure t)
+;; ****************************************************************
+;; GTD Configuration
+;; ****************************************************************
+(setq org-todo-keywords
+  '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)")))
+
+
 ;; link from clip
 (use-package org-cliplink
   :no-require
@@ -31,13 +40,18 @@
   :init
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-(setq org-default-notes-file "~/Documents/_org/notes.org")
 ;;; Set Org-Capture templates
 (setq org-capture-templates
-      `(("t" "todo" entry (file+headline "~/Documents/_org/tasks.org" "Tasks")
-         "** TODO %^{Task} %?")
-        ("n" "note" entry (file+headline "~/Documents/_org/notes.org")
-         "* %? :NOTE:\n%U\n%a\n" :clock-resume t)))
+      '(("t" "todo" entry
+         (file+headline "~/Documents/_org/tasks.org" "Tasks")
+         "** TODO %^{Brief description} %^g\n%?\nAdded: %U")
+        ("n" "note" entry
+         (file+headline "~/Documents/_org/notes.org" "Notes")
+         "** :NOTE:\n%U\n%a\n" :clock-keep t)
+        ("h" "habit" entry
+         (file+headline "~/Documents/_org/tasks.org" "Habits")
+         "** TODO %^{Brief description}%^t")))
+
 
 (define-key global-map (kbd "C-c l") 'org-store-link)
 (define-key global-map (kbd "C-c c") 'org-capture)
@@ -82,15 +96,44 @@
 ;;; integrate projectile todos with org-todos
 (use-package org-projectile
   :after (org projectile)
-  :bind (("C-c n p" . org-projectile-project-todo-completing-read)
-         ("C-c c" . org-capture))
+  :bind (("C-c n p" . org-projectile-project-todo-completing-read))
   :config
   (progn
     (setq org-projectile-projects-file
           "~/Documents/_org/projects.org")
     (setq org-agenda-files (append org-agenda-files (org-projectile-todo-files)))
     (push (org-projectile-project-todo-entry) org-capture-templates)))
-
+;; Supercharged agenda
+(use-package org-super-agenda
+  :disabled t
+  ;; :preface
+  ;; (defun super-jump-to-org-agenda ()
+  ;;   (interactive)
+  ;;   (let ((org-super-agenda-groups
+  ;;          '((:name "Today"
+  ;;                   :time-grid t
+  ;;                   :todo "TODAY")
+  ;;            (:name "Important"
+  ;;                   :tag "bills"
+  ;;                   :priority "A")
+  ;;            (:order-multi
+  ;;             (2 (:name "Shopping in town"
+  ;;                       :and (:tag "shopping" :tag "@town"))
+  ;;                (:name "Food-related"
+  ;;                       :tag ("food" "dinner"))
+  ;;                (:name "Personal"
+  ;;                       :habit t
+  ;;                       :tag "personal")
+  ;;                (:name "Space-related (non-moon-or-planet-related)"
+  ;;                       :and (:regexp ("space" "NASA")
+  ;;                                     :not (:regexp "moon" :tag "planet")))))
+  ;;            (:todo "WAITING" :order 8)
+  ;;            (:todo ("SOMEDAY" "TO-READ" "CHECK" "TO-WATCH" "WATCHING")
+  ;;                   :order 9)
+  ;;            (:priority<= "B" :order 1))))
+  ;;     (org-agenda nil "a")))
+  :config
+  (org-super-agenda-mode))
 ;; Calendar packages for org
 (use-package calfw)
 (use-package calfw-org
