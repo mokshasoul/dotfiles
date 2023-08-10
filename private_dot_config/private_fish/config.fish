147772ce26cf50f9198ad762f6cbb0a2cb58aefd
Charis-Nicolas Georgiou <cng_it@posteo.net>
2023-08-10 20:34:04 +0200

    feat: check latest fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,42 +1,36 @@
 set fish_greeting
 
-set -q XDG_CONFIG_HOME; or set -U XDG_CONFIG_HOME ~/.config
+# XDG Stuff
+set -q XDG_CONFIG_HOME; or set -U XDG_CONFIG_HOME "$HOME/.config"
+set -q XDG_DATA_HOME; or set -U XDG_DATA_HOME "$HOME/.local/share"
+set -q XDG_CACHE_HOME; or set -U XDG_CACHE_HOME "$HOME/.cache"
+
 # Docker speed-ups
 set -x COMPOSE_DOCKER_CLI_BUILD 1
 set -x DOCKER_BUILDKIT 1
 
 set -x LANG en_IE.UTF-8
 set -x LC_ALL en_IE.UTF-8
-set -x GOPATH $HOME/.go
+
+set -x GOPATH "$HOME/.go"
 set -x BAT_THEME GitHub
 
+# nnn-Configuration
 set -x NNN_COLORS 11112
 set -x NNN_FCOLORS 0B0B04060006060009060B06
+
 set -x RIPGREP_CONFIG_PATH $XDG_CONFIG_HOME/ripgrep/config
 set -x EDITOR "nvim -f"
 
 if test -f "$HOME/Applications/Neovide.app/Contents/MacOS/neovide"
-    set -x VISUAL "$HOME/Applications/Neovide.app/Contents/MacOS/neovide --nofork"
+    # set -x VISUAL "$HOME/Applications/Neovide.app/Contents/MacOS/neovide --nofork"
+    set -x VISUAL $EDITOR
 end
 
 if test -e "$HOME/.1password/agent.sock"
     # on OSX run this first:
     # mkdir -p ~/.1password && ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
     set -x SSH_AUTH_SOCK "$HOME/.1password/agent.sock"
-end
-
-# Navigation
-function ..
-    cd ..
-end
-function ...
-    cd ../..
-end
-function ....
-    cd ../../..
-end
-function .....
-    cd ../../../..
 end
 
 alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
@@ -51,14 +45,13 @@ if test -d "$HOME/.cargo/bin"
     fish_add_path "$HOME/.cargo/bin"
 end
 
-if test -d $HOME/.local/bin
-    fish_add_path $HOME/.local/bin
+if test -d "$HOME/.local/bin"
+    fish_add_path "$HOME/.local/bin"
 end
 
-if test -d (go env GOPATH)/bin
-    fish_add_path (go env GOPATH)/bin
+if test -d "$(go env GOPATH)/bin"
+    fish_add_path "$(go env GOPATH)/bin"
 end
-
 
 set -x FZF_DEFAULT_OPTS "--height 50% --layout=reverse --border"
 if type -q fd
@@ -83,14 +76,6 @@ if type -q dog
     end
 end
 
-# Fuzzy find & edit
-function vp
-    if test (count $argv) -gt 0
-        command $EDITOR $argv
-    else
-        fzf -m | xargs $EDITOR
-    end
-end
 
 if status is-login
     pyenv init --path | source
@@ -102,10 +87,6 @@ if status is-interactive
 
     if type -q direnv
         direnv hook fish | source
-    end
-
-    if type -q pipenv
-        set pipenv_fish_fancy yes
     end
 
     if type -q navi
@@ -124,8 +105,9 @@ if status is-interactive
         pyenv init - | source
         pyenv virtualenv-init - | source
         if not set -q PYENV_ROOT
-           set -U PYENV_ROOT $HOME/.pyenv 
+            set -U PYENV_ROOT $HOME/.pyenv
         end
+        set -x PIPX_DEFAULT_PYTHON "/Users/charis/.pyenv/shims/python3"
     end
 
     if type -q brew
@@ -144,9 +126,8 @@ if status is-interactive
         fish_add_path (brew --prefix ruby)/bin
     end
 
-    if type -q hub
-        eval (hub alias -s)
+    if [ "$TERM_PROGRAM" = "iTerm.app" ]
+        test -e "$HOME/.iterm2_shell_integration.fish"; and source "$HOME/.iterm2_shell_integration.fish"
     end
 
-    test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 end
