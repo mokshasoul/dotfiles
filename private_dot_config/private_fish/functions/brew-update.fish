664825158493d21c10b34ea7a2613e7449f0996b
Charis-Nicolas Georgiou <cng_it@posteo.net>
2024-08-28 21:43:20 +0200

    fix(private_fish): update fnm + brew-update

--- private_dot_config/private_fish/functions/brew-update.fish
+++ private_dot_config/private_fish/functions/brew-update.fish
@@ -1,9 +1,9 @@
 function brew-update
-	brew -v update
-	brew upgrade --force-bottle
-	brew upgrade
-	brew cleanup --prune 60
-	brew doctor
-	npm-check -g -u
-	nix flake update --flake /Users/moksha/.config/nix-darwin
+    brew -v update
+    brew upgrade --force-bottle
+    brew upgrade
+    brew cleanup --prune 60
+    brew doctor
+    npm-check -g -u
+    nix flake update --flake $HOME/.config/nix-darwin
 end
