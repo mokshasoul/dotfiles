f7be3fd05c0e3460be3379ccf5aeec31dd0f7505
Charis-Nicolas Georgiou <cng_it@posteo.net>
2024-09-18 15:34:06 +0200

    fix(nvim): neovide fonts and scaling

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -3,7 +3,7 @@
 -- Add any additional options here
 
 if vim.g.neovide then
-  vim.o.guifont = "Fira Code Mono:h34"
+  vim.o.guifont = "FiraCode Nerd Font Mono:h34"
   vim.g.neovide_scale_factor = 0.5
 end
 
