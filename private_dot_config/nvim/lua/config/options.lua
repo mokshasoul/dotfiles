a6e1ec7f5a76b44b5cd76dc956ddba8e5c49a1bd
Charis-Nicolas Georgiou <cng_it@posteo.net>
2024-11-19 09:25:20 +0100

    fix(nvim): auto darkmode

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -6,15 +6,26 @@ if vim.g.neovide then
   vim.o.guifont = "FiraCode Nerd Font Mono:h34"
   vim.g.neovide_scale_factor = 0.5
 end
+--
+-- Check macOS light / dark user interface state and return theme accordingly
+local function getPreferredTheme()
+    local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
 
-if os.getenv("OS_APPEARANCE") == "Dark" then
-  vim.go.background = "dark"
-  vim.o.background = "dark"
-else
-  vim.go.background = "light"
-  vim.o.background = "light"
+    if not handle then
+        return "light"
+    end
+
+    local result = handle:read("*a")
+    handle:close()
+
+    if result and result:match("Dark") then
+        return "dark" -- Dark theme
+    else
+        return "light" -- Light theme
+    end
 end
 
-if vim.g.vscode then
-  vim.opt.incsearch = false
-end
+-- Apply the preferred theme
+local preferred_theme = getPreferredTheme()
+vim.cmd.set(string.format("bg=%s",preferred_theme))
+vim.print("setting colorscheme")
