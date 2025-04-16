6a8efdb6413e6767bec79a154285758647b9dada
Charis-Nicolas Georgiou <cng_it@posteo.net>
2025-04-16 23:29:33 +0200

    remove wezterm types/add librarys same way as folke

--- private_dot_config/nvim/lua/plugins/tmp.lua
+++ private_dot_config/nvim/lua/plugins/tmp.lua
@@ -1,6 +1,6 @@
 -- taken from folke: https://github.com/folke/dot/blob/master/nvim/lua/plugins/tmp.lua
 return {
-  { "justinsgithub/wezterm-types", lazy = true },
+  -- { "justinsgithub/wezterm-types", lazy = true },
   { "LuaCATS/luassert", name = "luassert-types", lazy = true },
   { "LuaCATS/busted", name = "busted-types", lazy = true },
   { "godlygeek/tabular", lazy = true, cmd = "Tabularize" },
@@ -10,9 +10,9 @@ return {
       opts.debug = true
       -- opts.runtime = "~/projects/neovim/runtime"
       vim.list_extend(opts.library, {
-        { path = "wezterm-types", mods = { "wezterm" } },
-        { path = "luassert-types/library", words = { "assert" } },
-        { path = "busted-types/library", words = { "describe" } },
+        -- { path = "wezterm-types", mods = { "wezterm" } },
+        { path = "${3rd}/luassert/library", words = { "assert" } },
+        { path = "${3rd}/busted/library", words = { "describe" } },
       })
     end,
   },
