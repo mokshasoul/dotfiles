87705bd9eeb72f1ad84306956dbf1249dd914f51
Charis-Nicolas Georgiou <cng_it@posteo.net>
2025-09-08 20:35:33 +0300

    refactor(nvim): copilot reduce affinity

---
+++ private_dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "saghen/blink.cmp",
+    dependencies = { "giuxtaposition/blink-cmp-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-cmp-copilot",
+            kind = "Copilot",
+            score_offset = -100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
+}
