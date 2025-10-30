92751adda6420c05110ee888794c8f714c6300ec
Charis-Nicolas Georgiou <cng_it@posteo.net>
2025-10-30 01:34:45 +0100

    re-add updated dotfiles

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -12,9 +12,17 @@
 # Note: All major configuration has been moved to organized conf.d files
 # Add any machine-specific or temporary configuration below if needed
 if status is-interactive
-    # Theme management
-    update_theme
-    
+
+    set -l theme_result (osascript -e 'tell application "System Events" to tell appearance preferences to return dark mode' 2>/dev/null)
+    if test "$theme_result" = true
+        set -gx OS_APPEARANCE Dark
+    else
+        set -gx OS_APPEARANCE Light
+    end
     # FZF key bindings (needs to be in config.fish where functions are loaded)
     fzf_configure_bindings --directory=\ct
 end
+
+# >>> JVM installed by coursier >>>
+set -gx JAVA_HOME "/Users/moksha/Library/Caches/Coursier/arc/https/github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.8%252B9/OpenJDK21U-jdk_aarch64_mac_hotspot_21.0.8_9.tar.gz/jdk-21.0.8+9/Contents/Home"
+# <<< JVM installed by coursier <<<
