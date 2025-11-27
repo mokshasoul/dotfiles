ff76941f03b70b3e13f9ec590e1458122c0ed0a9
Charis-Nicolas Georgiou <cng_it@posteo.net>
2025-11-27 18:25:07 +0100

    chore: update config

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -5,38 +5,41 @@
 --   https://www.hammerspoon.org/docs/hs.audiodevice.html
 --   https://www.hammerspoon.org/docs/hs.audiodevice.watcher.html
 --
-
+-- Hammerspoon globals
+hs.loadSpoon("EmmyLua")
 local MICROPHONE_DEVICE_NAME = "AppleUSBAudioEngine:BLUE MICROPHONE:Blue Snowball :SBIrzW:1"
 local FALLBACK_DEVICE_NAME = "BuiltInMicrophoneDevice"
 
 local log = hs.logger.new("init", "debug")
 log.i("Initializing")
-
-function audioDeviceCallback(event)
+function AudioDeviceCallback(event)
   log.f('audioDeviceCallback: "%s"', event)
-  if event ~= "dIn" then -- Remove the trailing space, it's a bug
-    return
-  end
+  -- if event ~= "dIn" then
+  --   return
+  -- end
 
   local defaultInputDevice = hs.audiodevice.defaultInputDevice()
-  log.f("Input device has changed to %s", defaultInputDevice:name())
+
+  if defaultInputDevice ~= nil then
+    log.f("Input device has changed to %s", defaultInputDevice:name())
+  end
 
   local microphone = hs.audiodevice.findDeviceByUID(MICROPHONE_DEVICE_NAME)
-  if microphone then
+  if microphone ~= nil then
     log.i("Setting microphone to be the default again")
     microphone:setDefaultInputDevice()
   else
+    log.i("Microphone is not connected, using fallback")
     local fallback = hs.audiodevice.findDeviceByUID(FALLBACK_DEVICE_NAME)
-    if fallback then
+    if fallback ~= nil then
       fallback:setDefaultInputDevice()
-      log.w("Microphone is not connected, using fallback")
     else
       log.e("Neither microphone nor fallback device found!")
     end
   end
 end
 
-hs.audiodevice.watcher.setCallback(audioDeviceCallback)
+hs.audiodevice.watcher.setCallback(AudioDeviceCallback)
 hs.audiodevice.watcher.start()
 
 log.i("Initialized!")
@@ -47,22 +50,3 @@ hs.autoLaunch(true)
 hs.menuIcon(false)
 hs.automaticallyCheckForUpdates(true)
 hs.window.animationDuration = 0
-
---------------------------------------------------------------------------------
--- LOAD MODULES
-
-G = {} -- persist from garbage collector
-
----Try to require the module, and do not error when one of them cannot be
----loaded, but do notify if there was an error.
----@param module string module to load
-local function safeRequire(module)
-  local success, M = pcall(require, module)
-  G[module:sub(5)] = M
-  if not success then
-    hs.alert.show(M, 5)
-    print(M)
-  end
-end
-
-safeRequire("apps.app-specific-behavior")
--- private_dot_config/nvim/lua/plugins/tmp.lua
+++ private_dot_config/nvim/lua/plugins/tmp.lua
@@ -6,14 +6,17 @@ return {
   { "godlygeek/tabular", lazy = true, cmd = "Tabularize" },
   {
     "folke/lazydev.nvim",
-    opts = function(_, opts)
-      opts.debug = true
-      -- opts.runtime = "~/projects/neovim/runtime"
-      vim.list_extend(opts.library, {
+    opts = {
+      library = {
         -- { path = "wezterm-types", mods = { "wezterm" } },
         { path = "${3rd}/luassert/library", words = { "assert" } },
         { path = "${3rd}/busted/library", words = { "describe" } },
-      })
-    end,
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "LazyVim", words = { "LazyVim" } },
+      },
+      enabled = function(root_dir)
+        return not vim.uv.fs_stat(root_dir .. "/.luarc.json")
+      end,
+    },
   },
 }
