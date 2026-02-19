0e190f225864e3eb40c660f9ee34439be2354a8f
Charis-Nicolas Georgiou <cng_it@posteo.net>
2026-02-19 20:54:36 +0100

    fix(private_fish): function uses native fish stuff

--- private_dot_config/private_fish/functions/rename-files-lowercase.fish
+++ private_dot_config/private_fish/functions/rename-files-lowercase.fish
@@ -1,6 +1,5 @@
 function rename-files-lowercase -d "#rename all files to lowercase"
-    for f in * do 
-			mv "$f" "$f.tmp"
-			mv "$f.tmp" "$(echo $f | tr "[:upper:]" "[:lower:]")"
+    for f in * do
+        mv "$f" (string lower $f)
     end
 end
