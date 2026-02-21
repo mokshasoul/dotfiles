c76127d7e5c83236c01bc53194c9a88e7d5a2bec
Charis-Nicolas Georgiou <cng_it@posteo.net>
2026-02-21 03:49:05 +0100

    chore(private_fish): add more stuff to fish

---
+++ private_dot_config/private_fish/functions/lazygit.fish
@@ -0,0 +1,7 @@
+function lazygit --wraps lazygit
+    set -f conf_file "$HOME/.config/lazygit/config.yml"
+    if test (string lower "$OS_APPEARANCE") = light
+        set -f conf_file "$HOME/.config/lazygit/config-light.yml"
+    end
+    command lazygit --use-config-file $conf_file
+end
