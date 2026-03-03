9b82baa6ead6086d8258a3a858c83fce0e866220
Charis-Nicolas Georgiou <cng_it@posteo.net>
2026-03-03 22:15:50 +0100

    maintenance

--- private_dot_config/bat/run_once_after_update-bat-theme.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-curl -Lo "$(bat --config-dir)/themes/Catppuccin Latte.tmTheme" \
-  https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme
-bat cache --build
