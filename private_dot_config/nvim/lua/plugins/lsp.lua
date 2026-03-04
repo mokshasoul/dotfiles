e5245e024e69b34da367ebaddcf1058f5092fe59
Charis-Nicolas Georgiou <charis.georgiou@soundcloud.com>
2026-03-04 10:44:29 +0100

    chore(nvim): remove nix lsp

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,24 @@
 return {
   {
-    "stevearc/conform.nvim",
+    "mfussenegger/nvim-lint",
     optional = true,
-    opts = {
-      formatters_by_ft = {
-        ["nix"] = { "alejandra" },
-      },
-    },
+    opts = function(_, opts)
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      opts.linters_by_ft.markdown = { "markdownlint-cli2" }
+
+      opts.linters = opts.linters or {}
+      opts.linters["markdownlint-cli2"] = {
+        -- Disable stdin to enable config file discovery.
+        -- markdownlint-cli2 traverses directories to find .markdownlint.json,
+        -- .markdownlint-cli2.jsonc, etc., but this only works with file paths.
+        stdin = false,
+        args = {},
+        parser = require("lint.parser").from_errorformat("%f:%l:%c %m,%f:%l %m", {
+          source = "markdownlint",
+          severity = vim.diagnostic.severity.WARN,
+        }),
+      }
+    end,
   },
   {
     "neovim/nvim-lspconfig",
