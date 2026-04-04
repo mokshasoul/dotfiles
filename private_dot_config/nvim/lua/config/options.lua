4b7ebddf35205b178d7bc3c9bc0f2c3f452b7d75
Charis-Nicolas Georgiou <cng_it@posteo.net>
2026-04-04 14:04:49 +0200

    refactor: remove stuff that are unused

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -2,10 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-if vim.g.neovide then
-  vim.o.guifont = "CommitMono Nerd Font Propo:h34"
-  vim.g.neovide_scale_factor = 0.5
-end
 PREFERRED_THEME = "light"
 
 local uname = io.popen("uname -s")
