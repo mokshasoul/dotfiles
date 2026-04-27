6f5a3097894a20d07c7aed8655e5fd3142574cdc
Charis-Nicolas Georgiou <cng_it@posteo.net>
2026-04-28 01:49:38 +0200

    set zsh as login shell --> exec fish

--- private_dot_config/private_fish/conf.d/01_brew.fish
+++ private_dot_config/private_fish/conf.d/01_brew.fish
@@ -3,15 +3,15 @@ if test -d /opt/homebrew/bin
     set -gx HOMEBREW_CELLAR $HOMEBREW_PREFIX/Cellar
     set -gx HOMEBREW_REPOSITORY $HOMEBREW_PREFIX
 
-    set -gx HOMEBREW_BAT 7
+    set -gx HOMEBREW_BAT 1
     set -gx HOMEBREW_NO_AUTO_UPDATE 1
     set -l completions "$HOMEBREW_PREFIX/share/fish/completions"
 
     fish_add_path $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
 
-    # if test -n "$MANPATH[1]"
-    #     set --global --export MANPATH '' $MANPATH
-    # end
+    if test -n "$MANPATH[1]"
+        set -gx MANPATH ''
+    end
 
     # if not contains "$HOMEBREW_PREFIX/share/info" $INFOPATH
     #     set --global --export INFOPATH /opt/homebrew/share/info $INFOPATH
@@ -19,8 +19,4 @@ if test -d /opt/homebrew/bin
     if test -d "$HOMEBREW_PREFIX/share/fish/completions" && not contains $completions $fish_complete_path
         set -a fish_complete_path $completions
     end
-
-    # if test -d "$HOMEBREW_PREFIX/share/fish/vendor_completions.d" && not contains $vendor_comp_dir $fish_complete_path
-    #     set -a fish_complete_path $vendor_comp_dir
-    # end
 end
