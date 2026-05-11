4737b7ace0042aa6ed6c1357e0a886094c227745
Charis-Nicolas Georgiou <cng_it@posteo.net>
2026-05-11 11:13:29 +0200

    chore: updates

--- private_dot_config/private_fish/conf.d/04_aliases.fish
+++ private_dot_config/private_fish/conf.d/04_aliases.fish
@@ -1,16 +1,20 @@
-if status is-interactive
-    abbr --add gst git status
-    abbr --add gco git checkout
-    abbr --add gcb git checkout -b
-    abbr --add gd git diff
-    abbr --add gds git diff --staged
-    abbr --add glog git log --oneline --graph --decorate
-    abbr --add grb git rebase
-    abbr --add gsh git stash
-    abbr --add gsp git stash pop
-    abbr --add gca git commit --amend
-    abbr --add gcp git cherry-pick
-    abbr --add gf git fetch --all --prune
-    abbr --add gp git push
-    abbr --add gl git pull
-end
+abbr --add gst git status
+abbr --add gco git checkout
+abbr --add gcb git checkout -b
+abbr --add gd git ddiff
+abbr --add gds git diff --staged
+abbr --add glog git log --oneline --graph --decorate
+abbr --add grb git rebase
+abbr --add gsh git stash
+abbr --add gsp git stash pop
+abbr --add gca git commit --amend
+abbr --add gcp git cherry-pick
+abbr --add gf git fetch --all --prune
+abbr --add gp git push
+abbr --add gl git pull
+abbr --add ... cd ../..
+abbr --add .. 'cd ..'
+abbr --add ... 'cd ../..'
+abbr --add .... 'cd ../../..'
+abbr --add ..... 'cd ../../../..'
+abbr --add ...... 'cd ../../../../..'
